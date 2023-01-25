7fc83581e8919c2659b0e32b019120dd59d613ed
Muaz Waqas <mw2283@york.ac.uk>
2023-01-25 12:44:43 +0000

    New Tutorial

--- core/src/com/badlogic/drop/Sprites/CuttingStation.java
+++ core/src/com/badlogic/drop/Sprites/CuttingStation.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class CuttingStation extends InteractiveTileObject {
+    //Interactive Tile Object effectively acts as the parents class
     public CuttingStation(World world, TiledMap map, Rectangle bounds) {
         super(world, map, bounds);
     }
